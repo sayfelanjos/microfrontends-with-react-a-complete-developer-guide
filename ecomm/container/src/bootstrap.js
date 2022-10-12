b0b0e99f2f37bb827fccd914c1049fdd195ba9f4
Saymon Felipe <saymonfelipe@hotmail.com>
2022-10-11 22:10:37 -0300

    Remove node_modules

--- ecomm/container/src/bootstrap.js
+++ ecomm/container/src/bootstrap.js
@@ -1,4 +1,6 @@
-import 'products/ProductsIndex';
-import 'cart/CartShow';
+import { mount as productMount } from 'products/ProductsIndex';
+import { mount as cartMount } from 'cart/CartShow';
 
 console.log("Container!!!");
+productMount(document.querySelector('#dev-products'));
+cartMount(document.querySelector('#dev-cart'));
