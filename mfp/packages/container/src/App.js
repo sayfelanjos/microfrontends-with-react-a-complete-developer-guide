da77cb9cb99066187b651c947b4b8782662f8d76
Saymon Felipe <saymonfelipe@hotmail.com>
2022-10-24 14:11:57 -0300

    Added marketing deployment

--- mfp/packages/container/src/App.js
+++ mfp/packages/container/src/App.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import { mount } from 'marketing/MarketingApp';
 
 import MarketingApp from "./Components/MarketingApp";
 
 export default () => {
     return <div>
-        <h1>Hi there!!!</h1>
-        <hr />
         <MarketingApp />
     </div>
 };
